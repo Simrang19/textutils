bdd0f23bd4f75c3517bd39c0fc39ce6517669998
Simran <simran.simrangupta@gmail.com>
2022-03-20 09:35:28 +0530

    almost ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import { useState } from "react";
 import Alert from "./components/Alert";
-import About from "./components/About";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import About from "./components/About";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light"); //whether dark mode enabled or not
@@ -34,27 +34,27 @@ function App() {
 
   return (
     <>
-      <Router>
+      {/* <Router> */}
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert} />
       <div className="container my-3">
-        <Switch>
-          <Route exact path="/about">
+        {/* <Switch> */}
+          {/* <Route exact path="/about">
             <About />
           </Route>
           
-          <Route exact path="/">
+          <Route exact path="/"> */}
           <TextForm
           showAlert={showAlert}
           heading="Enter the text to analyze below"
           mode={mode}
           />
           
-          </Route>
-      </Switch>
+          {/* </Route> */}
+      {/* </Switch> */}
         {/* <About/> */}
       </div>
-      </Router>
+      {/* </Router> */}
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 export default function (props) {
+    
     const handleUpClick=()=>{
         let newText=text.toUpperCase();
         setText(newText)
@@ -24,8 +25,9 @@ export default function (props) {
     const handleCopy=()=>{
         var text=document.getElementById("myBox");
         text.select();
-        text.setSelectionRange(0,9999);
+        // text.setSelectionRange(0,9999);
         navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
         props.showAlert("Copied the text to Clipboard!","success");
     }
     const handleExtraSpaces=()=>{
@@ -42,20 +44,20 @@ export default function (props) {
         <div className='container' style={{color:props.mode==='dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" value={text} onChange ={handleOnChange} id="myBox" rows="8" style={{backgroundColor:props.mode==='light'?'white':'grey', color:props.mode==='dark'?'white':'black'}}></textarea>
+            <textarea className="form-control" value={text} onChange ={handleOnChange} id="myBox" rows="8" style={{backgroundColor:props.mode==='light'?'white':'#13466e', color:props.mode==='dark'?'white':'black'}}></textarea>
             </div>
-            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
-            <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
-            <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
-            <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleClearClick}>Clear Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleCopy}>Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         </div>
         <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
             <h1>You text summary</h1>
-            <p>{text.split(" ").length} words and {text.length} characters</p>
-            <p>{0.008*text.split(" ").length} Minutes read</p>
+            <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+            <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
             <h2>Preview</h2>
-            <p>{text.length>0?text:"Enter Text in the textbox above to preview here"}</p>
+            <p>{text.length>0?text:"Nothing to preview"}</p>
         </div>
         </>
   );
